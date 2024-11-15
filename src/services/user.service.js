82d9810a20056a5e416ec47969d3e721c12299c7
sehwanii_cau <sehwan1023@gmail.com>
2024-11-15 05:15:33 +0000

    Refactor : fix response form

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -3,11 +3,11 @@ import  {UserNotFoundError} from "../errors.js"
 
 export const getUserInfo = async (userId) => {
     if (isNaN(userId)){
-      throw new UserNotFoundError("사용자 ID 형식이 잘못되었습니다.", userId);
+      throw new UserNotFoundError("사용자 ID 형식이 잘못되었습니다.", {"userId" : userId});
     }
     const userInfo = await findUserInfo(userId);
     if (!userInfo) {
-      throw new UserNotFoundError("사용자를 찾을 수 없습니다.", userId);
+      throw new UserNotFoundError("사용자를 찾을 수 없습니다.", {"userId" : userId});
     }
     return userInfo;
 };
