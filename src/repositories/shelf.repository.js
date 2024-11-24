ef05da69d14582a18077a5b6120a98f1aaa1d76b
sehwanii_cau <sehwan1023@gmail.com>
2024-11-24 12:49:37 +0000

    Refactor : remove annotation

--- src/repositories/shelf.repository.js
+++ src/repositories/shelf.repository.js
@@ -1,14 +1,5 @@
 import { prisma } from "../db.config.js";
 
-// export const countShelves = async () => {
-//     try {
-//         const shelfNum = await prisma.shelf.count();
-//         return shelfNum;
-//     } catch (error){
-//         console.error('Error counting shelves:', error);
-//     }
-// };
-
 export const countShelves = async () => {
     // 데이터베이스에서 책장 개수 조회
     const shelfCount = await prisma.shelf.count();
